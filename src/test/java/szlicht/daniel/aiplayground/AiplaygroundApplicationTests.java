468132fd3ad44bfb53167b78b11bb027a8317939
Daniel Szlicht <daniel.szlicht25@gmail.com>
2025-06-06 12:17:41 +0200

    Initial commit

---
+++ src/test/java/szlicht/daniel/aiplayground/AiplaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package szlicht.daniel.aiplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiplaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
